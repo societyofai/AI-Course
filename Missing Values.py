674c5dce41efeeac0e948ab6012a6c499d510c4f
societyofai <68428178+societyofai@users.noreply.github.com>
2021-06-11 20:06:21 +0530

    Add files via upload

---
+++ Missing Values.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import pandas as pd
+
+df=pd.read_csv('Data.csv')
+print("\n")
+
+print(df.columns)
+
+print("\n")
+
+print(df.shape)
+
+print("\n")
+
+print(df.corr())
+print("\n")
+
+print("Max Salary value is in row number: ",df['Salary'].idxmax()) #it will print the  row number of maximum value in that column
+
+print("Salary to age ratio is",df['Salary']/df['Age'])
+
+print("\n")
+print(df.isnull().sum())
+
+print("\n")
+print(df['Salary'].isnull())
+
+
+#method 1
+
+df.loc[4,'Salary']=40000
+
+print(df)
+
+#method 2
+
+df['Age'].fillna(45,inplace=True)
+
+print(df)
+
+#method 3
+
+df.dropna(thresh=3)
+
+
+#Segregation of input and output variables
+
+X=df.iloc[:,0:3].values
+y=df.iloc[:,3].values
+
+
+#method 4
+
+from sklearn.impute import SimpleImputer
+imputer=SimpleImputer(missing_values=np.nan,strategy='mean')
+imputer.fit(X[:,1:3])
+X[:,1:3]=imputer.transform(X[:,1:3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
